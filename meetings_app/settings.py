962c9ab09c2cd3ec1a3196810a2a1c3ca6833add
Nuethera <vaishnavj2124@outlook.com>
2023-09-25 02:05:51 +0530

    vercel ready 2

--- meetings_app/settings.py
+++ meetings_app/settings.py
@@ -14,8 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -66,12 +65,8 @@ WSGI_APPLICATION = 'meetings_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': os.environ.get("PBPASS"),
-        'HOST': os.environ.get("PBHOST"),
-        'PORT':  os.environ.get("PBPORT"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -121,5 +116,3 @@ STATICFILES_DIRS =[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
